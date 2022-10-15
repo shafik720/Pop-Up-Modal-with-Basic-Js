e5687417f5fe9cb49c8f009f5f5a6d743a71cb0a
shafik720 <shafikrasel5@gmail.com>
2022-10-15 16:21:55 +0600

    dfgfdsg

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const modalButton = document.querySelector('.wrapper button'),
 section = document.querySelector('section'),
+crossBtn = document.querySelector('.cross'),
 modalParent = document.querySelector('.modal-parent')
 ;
 
@@ -8,4 +9,9 @@ modalParent = document.querySelector('.modal-parent')
 modalButton.addEventListener('click',()=>{
     modalParent.style.display = 'block';
     section.style.filter = "blur(10px)"
+})
+
+crossBtn.addEventListener('click',()=>{
+    modalParent.style.display = 'none';
+    section.style.filter = "blur(0)"
 })
