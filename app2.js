e5d14357db1d9c1b22e21889a567022cba3fb05a
shafik720 <shafikrasel5@gmail.com>
2022-10-15 16:50:47 +0600

    dsfdsfr

--- app2.js
+++ app2.js
@@ -1,10 +1,23 @@
 
 const modalParent = document.querySelector('.modal-parent'),
 section = document.querySelector('section'),
+crossBtn = document.querySelector('.cross'),
 button = document.querySelector('.wrapper button')
 ;
 
 button.addEventListener('click',()=>{
     modalParent.style.display = 'block';
     section.style.filter = 'blur(10px)'
+})
+
+crossBtn.addEventListener('click',()=>{
+    modalParent.style.display = 'none';
+    section.style.filter = 'blur(0)';
+})
+
+modalParent.addEventListener('click',(e)=>{
+    if(e.target.className == 'modal-parent'){
+        modalParent.style.display = 'none';
+        section.style.filter = 'blur(0)';
+    }
 })
